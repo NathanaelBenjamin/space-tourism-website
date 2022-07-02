fbfc93f3335007d63345b8f33d4b7921515c9716
Nathanael Benjamin <oluwakayodenathanael@gmail.com>
2022-07-02 04:29:44 +0100

    Carousel edited.

--- src/Pages/CREW/Crew.js
+++ src/Pages/CREW/Crew.js
@@ -19,7 +19,7 @@ const Crew = () => {
             <h5>MEET YOUR CREW</h5>
         </div>
 
-        <Carousel showArrows={false} autoPlay={true} infiniteLoop={true} centerMode={false} interval={5000}>
+        <Carousel showArrows={false} autoPlay={true} infiniteLoop={true} centerMode={false} interval={5000} swipeable={false}>
           {
             members.map(person => {
               return <MemberImage 
